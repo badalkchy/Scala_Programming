28485e00540e183b57ca46affb0504c027149753
Badal Chaudhary <87421888+badalkchy@users.noreply.github.com>
2023-01-23 23:00:41 -0800

    Add files via upload

---
+++ 1_Introduction/C_1_Conditional_loop.scala
@@ -0,0 +1,13 @@
+// For Loop
+
+import scala.io.StdIn.readInt;
+object C_1_Conditional_loop {
+  def main(Args:Array[String]):Unit={
+
+    print("Enter the number :");
+    var n:Int=readInt;
+    for(a<-1 to n){
+      print(a+" ");
+    }
+  }
+}
