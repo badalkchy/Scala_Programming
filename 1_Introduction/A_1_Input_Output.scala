2c76c4fc32e2a63cab7b5017c2b2d4d0176f0595
Badal Chaudhary <87421888+badalkchy@users.noreply.github.com>
2023-01-23 22:59:40 -0800

    Add files via upload

---
+++ 1_Introduction/A_1_Input_Output.scala
@@ -0,0 +1,7 @@
+import scala.io.StdIn.readInt;        // importing scala I/O package for input purpose
+object A_1_Input_Output {
+  def main(Args:Array[String]):Unit={
+    print("Enter the number :");
+    var n:Int=readInt();  // readInt() is a function use to take input from user
+  }
+}
